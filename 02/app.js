a07b937ad1b073dc8f4d2367e2986b8739c38602
kms278711 <kms278711@naver.com>
2022-03-10 16:56:03 +0900

    02생성

---
+++ 02/app.js
@@ -0,0 +1,36 @@
+var name = "kms";
+name= "111";
+console.log(name);
+
+
+function log(str) {
+    console.log(str);
+}
+
+log("hello");
+
+//object
+var img = {};
+img.width = 100;
+img.height = 200;
+console.log(img);
+
+//배열
+
+var arr = [1,2,3,4,5];
+console.log(arr);
+
+console.log(1+"1");
+console.log(name+"hello");
+
+
+var me = {
+    age : 25,
+    name : 'kms',
+    isMan: true,
+    sayHello: function(){
+        console.log('hello');
+    },
+};
+
+me.sayHello();
