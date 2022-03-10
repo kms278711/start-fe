a898e858ae37e04cec58ca6ff460fe98ee93944b
kms278711 <kms278711@naver.com>
2022-03-10 17:54:55 +0900

    작업물 마무리

--- 02/app.js
+++ 02/app.js
@@ -1,39 +1,73 @@
-var name = "kms";
-name= "111";
-console.log(name);
+// var name = "kms";
+// name= "111";
+// console.log(name);
 
 
-function log(str) {
-    console.log(str);
-}
+// debugger
+// function log(str) {
+//     console.log(str);
+// }
 
-log("hello");
+// log("hello");
 
-//object
-var img = {};
-img.width = 100;
-img.height = 200;
-console.log(img);
+// //object
+// var img = {};
+// img.width = 100;
+// img.height = 200;
+// console.log(img);
 
-//배열
+// //배열
 
-var arr = [1,2,3,4,5];
-console.log(arr);
+// var arr = [1,2,3,4,5];
+// console.log(arr);
 
-console.log(1+"1");
-console.log(name+"hello");
+// console.log(1+"1");
+// console.log(name+"hello");
 
 
-var me = {
-    age : 25,
-    name : 'kms',
-    isMan: true,
-    sayHello: function(){
-        console.log('hello');
-    },
-};
+// var me = {
+//     age : 25,
+//     name : 'kms',
+//     isMan: true,
+//     sayHello: function(){
+//         console.log('hello');
+//     },
+// };
 
-me.sayHello();
+// me.sayHello();
 
-var arr = [1,2,3,4,5];
-console.log(arr.length);
+// var arr = [1,2,3,4,5];
+// console.log(arr.length);
+
+// var article1 = {
+//     title: '제목1',
+//     url: 'http://naver.com',
+// };
+
+// var articles = [article1];
+// console.log(articles);
+
+var todos = ['운동'];
+
+var todo = '게임';
+todos.push(todo);
+
+// console.log(todos);
+
+todos.forEach(function(todo){
+    console.log(todo);
+});
+
+var updateTodo = '게임';
+var updateIndex = todos.findIndex(function(todos){
+    return todo === updateTodo;
+});
+console.log(updateIndex);
+
+
+var todos = ['운동', '공부'];
+var deleteTodo = '공부';
+var newTodos = todos.filter(function(todo) {
+  return todo !== deleteTodo;
+});
+console.log(newTodos);
